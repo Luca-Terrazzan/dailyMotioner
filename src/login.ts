69ed0fa43e19f22c80046475cb9ed3652553caa6
Luca Terrazzan <luca.terraz@gmail.com>
2017-11-19 19:10:49 +0100

    first get request poc

---
+++ src/login.ts
@@ -0,0 +1,7 @@
+import { Request } from 'request';
+
+Request('http://www.google.com', (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
