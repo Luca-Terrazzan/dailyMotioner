49af92e6acbab978b493a27fda292cf0f757d16a
Luca Terrazzan <luca.terrazzan@docebo.com>
2017-11-26 22:26:02 +0100

    added gulpfile es6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,17 +2,33 @@
 
 import gulp from 'gulp';
 import del from 'del';
-import typescript from 'typescript';
+import ts from 'gulp-typescript';
+import exec from 'child_process';
+import { getTrailingCommentRanges } from 'typescript';
+
+const tsProject = ts.createProject('./tsconfig.json');
 
 const path = {
     src: './src',
-    dist: './dist'
+    dist: './dist',
+    login: './dist' + '/login.js'
 };
 
-export const clean = () => del([path.dist]);
 
 gulp.task('clean', () => {
-    return gulp.src(path.dist)
-        .pipe(del);
+    del([path.dist + '/*']);
 });
 
+gulp.task(
+    'build', () => {
+        console.log('Building script...');
+        tsProject.src()
+            .pipe(tsProject())
+            .js.pipe(gulp.dest(path.dist));
+    }
+);
+
+gulp.task('run', ['build'],() => {
+    console.log('Executing script...');
+    exec.exec('node ' + path.login + ' > ' + path.dist + '/result.json');
+});
