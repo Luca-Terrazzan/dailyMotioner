57e47503831d42544090283520b1a02d696f2921
Luca Terrazzan <luca.terrazzan@docebo.com>
2017-11-26 22:49:08 +0100

    misc stuff

--- src/login.ts
+++ src/login.ts
@@ -3,6 +3,15 @@ import * as WebRequest from 'web-request';
 const token = 'Y3cCAhwAGlheCgdXU1AZFAUBBgoFFllfCwI';
 
 (async () => {
-    const result = await WebRequest.get('https://api.dailymotion.com/videos');
+    const url: string = 'https://api.dailymotion.com/user/me/likes?access_token=ODRaDBseAlRYQgdQXV5FHUdEEh0eUglZD00';
+    const header: object = {
+
+    };
+    const param: object = {
+        access_token: token
+    };
+
+    const result = await WebRequest.get(url);
+
     console.log(result.content);
 })();
