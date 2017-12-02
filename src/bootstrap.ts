b8480e560fab6810d0a98dfb55a65036d59297e0
Luca Terrazzan <luca.terraz@gmail.com>
2017-12-02 21:07:39 +0100

    improved bootstrapping

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import Logger from './lib/logger';
+import Video from './models/video.model';
 
 const logFolder = './logs/';
 const logTemplate = 'log';
@@ -11,7 +12,11 @@ const logExtension = '.log';
         fs.mkdirSync(logFolder)
     }
 
-    // might add random named logs later
+    // Create logger instance
     const logger = Logger.getInstance(fs.createWriteStream(logFolder + logTemplate + logExtension));
     Logger.info('Application bootstrapped!');
+
+    // Start the upload of test video
+    const video = new Video('Test video 1', 'a3UEAhFND1QKBBhZXQhBQ1UBGBdKEh0TQUo');
+    video.upload('../videos/test.mp4');
 })();
