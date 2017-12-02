f654d0a88c5b1a1ea9f077ab84a930ba9b9fe8bb
Luca Terrazzan <luca.terraz@gmail.com>
2017-12-02 17:23:48 +0100

    improved video model structure

--- src/models/video.model.ts
+++ src/models/video.model.ts
@@ -1,15 +1,48 @@
 import * as WebRequest from 'web-request';
+import Log from 'log';
 
-export class Video {
-    public upload(filepath: string, token: string): string {
+export default class Video {
+
+    public title: string;
+    public token: string;
+
+    public filepath: string;
+    public id: string;
+    public statusUrl: string;
+
+    constructor(title: string, token: string) {
+        this.title = title;
+        this.token = token;
+    }
+
+    public upload(filepath: string): string {
+        this.filepath = filepath;
+
+        const uploadUrl = this.getUploadUrl();
+
+        const videoUrl = this.uploadVideoToUploadUrl(uploadUrl);
+
+        this.postVideo(videoUrl);
+
         return 'uploaded video id';
     }
 
     private getUploadUrl(): string {
+        // ask for a upload url
+        // save progress polling url for later
+
         return 'someurl';
     }
 
-    private uploadVideoToUploadUrl(): boolean {
-        return false;
+    private uploadVideoToUploadUrl(uploadUrl: string): string {
+        // do the upload...
+
+        return 'loaded url';
+    }
+
+    private postVideo(url: string): string {
+        // post video to /me/videos
+
+        return 'video id';
     }
 }
