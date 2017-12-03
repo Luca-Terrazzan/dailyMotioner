f721832f1fb488f90f86e241fd5be3afc7564ad9
Luca Terrazzan <luca.terraz@gmail.com>
2017-12-03 11:54:08 +0100

    Update uploading process

--- src/login.ts
+++ src/login.ts
@@ -3,22 +3,22 @@ import Video from './models/video.model';
 
 const token = 'cHdZXQEQUE5GV1hLX1ZHQAkJCk4ORAcJWwE';
 
-(async () => {
-    const video = new Video('test', token, '../videos/test.mp4');
-    video.upload();
+// (async () => {
+//     const video = new Video('test', token, '../videos/test.mp4');
+//     video.upload();
 
-    const url = 'https://api.dailymotion.com/file/upload';
-    const header: object = {
-        Authorization: 'Bearer ' + token
-    };
-    const param: object = {
-        access_token: token
-    };
-    const body: object = {
-        file: '../videos/test.mp4'
-    };
+//     const url = 'https://api.dailymotion.com/file/upload';
+//     const header: object = {
+//         Authorization: 'Bearer ' + token
+//     };
+//     const param: object = {
+//         access_token: token
+//     };
+//     const body: object = {
+//         file: '../videos/test.mp4'
+//     };
 
-    const result = await WebRequest.get(url, {headers: header});
+//     const result = await WebRequest.get(url, {headers: header});
 
-    console.log(result.content);
-})();
+//     console.log(result.content);
+// })();
