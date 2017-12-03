1d018f22e88bac6cca89499948056ce316ac219f
Luca Terrazzan <luca.terrazzan@docebo.com>
2017-12-03 15:48:53 +0100

    Finalised POC on upload looping

---
+++ src/lib/uploader.ts
@@ -0,0 +1,50 @@
+import Video from '../models/video.model';
+import Logger from './logger';
+
+import * as fs from 'fs';
+import getVideoTitle from './utils';
+
+export default class Uploader {
+    private batchSize: number = 1;
+    private videosFolder: string;
+    private token: string;
+
+    private videos: {
+        title: string,
+        path: string
+    }[] = [];
+
+    constructor(batchSize: number, videosFolder: string, token: string) {
+        this.batchSize = batchSize;
+        this.videosFolder = videosFolder;
+        this.token = token;
+    }
+
+    public async init() {
+        const files = await fs.readdirSync(this.videosFolder);
+        Logger.debug('dir content: ', files);
+
+        for (const file of files) {
+            let title: string;
+            try {
+                title = getVideoTitle(file);
+            } catch (typeError) {
+                Logger.error(typeError);
+                continue;
+            }
+            this.videos.push({
+                title: title,
+                path: this.videosFolder + file
+            });
+        }
+        Logger.debug('current video: ', this.videos);
+    }
+
+    public async startVideoUpload() {
+        // Create video insance and start upload
+        Logger.debug('Starting uploading the following: ', this.videos);
+        for (const video of this.videos) {
+            (new Video(video.title, this.token, video.path)).upload();
+        }
+    }
+}
