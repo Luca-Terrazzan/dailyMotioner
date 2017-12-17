adc12371880f4dac1f56d23f85b7d07307224e19
Luca Terrazzan <luca.terraz@gmail.com>
2017-12-17 17:39:14 +0100

    Fixed bug on video title splitting
    There was a bug on how the title was parsed and didnt detect extensions if the filename had fulls tops in it

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export default function getVideoTitle(fileName: string): string {
     const fileData = fileName.split('.');
-    if (fileData[1] !== 'mp4') {
+    if (fileData.pop() !== 'mp4') {
         throw new TypeError('File ' + fileName + ' is not a valid mp4 file.');
     }
-    return fileData[0];
+    return fileData.join('.');
 }
